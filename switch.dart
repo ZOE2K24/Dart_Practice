51354a10ce890e6cbf1314a068dfac5d8d0458d0
ZOE2K24 <jm.legaspi06@gmail.com>
2025-03-12 14:02:32 -0600

    Create switch.dart

---
+++ switch.dart
@@ -0,0 +1,39 @@
+void main() {
+  
+String cat = "Cat";
+  
+// ternary operator
+  cat == "Cat" ? print("This is a cat!") :
+  print("This is not a cat.");
+  
+//Example of a switch statement
+  String day = '22';
+  
+  switch(day) {
+    case 'Monday':
+      print("Hey, it's Monday!");
+      break;
+    case 'Tuesday':
+      print("Hey, it's Tuesday!");
+      break;
+    case 'Wednesday':
+      print("Hey, it's Wednesday!");
+      break;
+    case 'Thursday':
+      print("Hey, it's Thursday!");
+      break;
+    case 'Friday':
+      print("Hey, it's Friday!");
+      break;
+    case 'Saturday':
+      print("Hey, it's Saturday!");
+      break;
+    case 'Sunday':
+      print("Hey, it's Sunday!");
+      break;
+    default:
+      print("This is most likely impossible");
+  }
+  
+}
+
